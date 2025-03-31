594c664755dfe1bf2e2b663637a6ff3c882cabfe
Einar671 <einarkaalhus2809@gmail.com>
2025-03-31 11:40:43 -0500

    Commit de Einar Kaalhus a Israel Orellana

--- src/main/java/ec/edu/ups/poo/principal/Principal.java
+++ src/main/java/ec/edu/ups/poo/principal/Principal.java
@@ -8,7 +8,7 @@ import java.util.GregorianCalendar;
 public class Principal {
     //El código funciona de manera correcta
     // Commit de Einar Kaalhus a Orellana Israel
-    //Transfomración
+    //SISISISIS NONONO AY LA
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         Persona personita = new Persona();
