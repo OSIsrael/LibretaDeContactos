34562333d4f4406d7cfca77d123546aba74156ed
Einar671 <einarkaalhus2809@gmail.com>
2025-03-31 12:39:47 -0500

    Commit para las reglas

--- src/main/java/ec/edu/ups/poo/principal/Principal.java
+++ src/main/java/ec/edu/ups/poo/principal/Principal.java
@@ -8,7 +8,7 @@ import java.util.GregorianCalendar;
 public class Principal {
     //El código funciona de manera correcta
     // Commit de Einar Kaalhus a Orellana Israel
-    //Penei
+
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         Persona personita = new Persona();
